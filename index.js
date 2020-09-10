59341488ba49f1671dfac67e43a7dd527d58b290
Jamie Lopez <jmelopez@outlook.com>
2020-09-10 16:16:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  for(let i= 0; i < 4; i++) {
+    console.log (`${musicians[i]} plays ${instruments[i]}.`)
+  }
+}
+theBeatlesPlay(musicians, instruments)
