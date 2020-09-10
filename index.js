99ec04859e0a46ac150f3b662e0fee50e61c1688
Jamie Lopez <jmelopez@outlook.com>
2020-09-10 16:53:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-function theBeatlesPlay(musicians, instruments) {
+var theBeatlesPlay = theBeatlesPlay(musicians,instruments) {
+  return function theBeatlesPlay(musicians, instruments) {
   for(let i= 0; i < 4; i++) {
     console.log (`${musicians[i]} plays ${instruments[i]}.`)
   }
+   }
+return theBeatlesPlay
 }
-theBeatlesPlay(musicians, instruments)
